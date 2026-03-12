83f190c74d81e657cb40af1948e737181e462494
A.Jackson <andrewz.jackson667@gmail.com>
2026-03-12 10:21:10 +0900

    Created HistorySection;

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,45 @@ import './App.css'
 export default function App() {
 
   return (
-    <>
+    <div className='app'>
       <Board />
-    </>
+      <HistorySection />
+    </div>
+  )
+}
+
+interface IPlayer {
+  name: string,
+  points: number,
+  history: Array<string>
+}
+
+function Player({ Player: IPlayer }) {
+  const [name, setName] = useState("");
+
+  function handleName() {
+    if (name === "") {
+      // Ooops! Need a Form to handle getting player data.
+    }
+  }
+
+  return (
+    <p>{Player.name}</p>
+  )
+}
+
+function HistorySection() {
+
+  return (
+    <div className='history'>
+      <h4>Game History</h4>
+      <div className='players-container'>
+        <p>Player 1</p>
+        <p>|</p>
+        <p>Player 2</p>
+      </div>
+      <p>________________________</p>
+    </div>
   )
 }
 
@@ -14,14 +50,14 @@ function Board() {
 
   return (
     <div className='board'>
-      <Row />
-      <Row />
-      <Row />
+      <ThreeSqRow />
+      <ThreeSqRow />
+      <ThreeSqRow />
     </div>
   )
 }
 
-function Row() {
+function ThreeSqRow() {
   return (
     <div className='board-row'>
       <Square />
@@ -33,14 +69,10 @@ function Row() {
 
 function Square() {
   const [value, setValue] = useState("");
+  // const [clickCnt, setClickCnt] = useState(0);
 
   function handleClick() {
-    if (value === 'X') {
-      setValue("");
-    }
-    else {
-      setValue('X');
-    }
+    setValue("X");
   }
   return (
     <button type='button' id='btn' className='sq' onClick={handleClick}>
